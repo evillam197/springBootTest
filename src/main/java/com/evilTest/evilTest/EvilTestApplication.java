05b279e9f665d3ace609d6deff200bd62d7d261b
Mario Ernesto Villacorta <evillam197@gmail.com>
2021-12-02 05:28:17 -0600

    Initial commit

---
+++ src/main/java/com/evilTest/evilTest/EvilTestApplication.java
@@ -0,0 +1,13 @@
+package com.evilTest.evilTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvilTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvilTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evilTest/evilTest/EvilTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evilTest.evilTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvilTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
